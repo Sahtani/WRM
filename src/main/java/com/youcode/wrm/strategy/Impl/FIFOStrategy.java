43cb29604806744b8856427e0d590210c158f168
Sahtani <soumia.sahtani@gmail.com>
2024-11-16 15:44:33 +0100

    refactor: Update Queue Management Strategies
    - Improved  implementation to enhance queue processing.
    - Updated  to refine the prioritization logic of visitors in the queue.

--- src/main/java/com/youcode/wrm/strategy/Impl/FIFOStrategy.java
+++ src/main/java/com/youcode/wrm/strategy/Impl/FIFOStrategy.java
@@ -2,7 +2,6 @@ package com.youcode.wrm.strategy.Impl;
 
 import com.youcode.wrm.entity.Visit;
 import com.youcode.wrm.strategy.QueueStrategy;
-import org.springframework.boot.autoconfigure.web.WebProperties;
 
 import java.util.Comparator;
 import java.util.List;
@@ -10,9 +9,9 @@ import java.util.List;
 public class FIFOStrategy implements QueueStrategy {
     @Override
     public Visit getNextVisit(List<Visit> waitingVisits) {
-            return waitingVisits.stream()
-                    .min(Comparator.comparing(Visit::getArrivalTime))
-                    .orElse(null);
-        }
+        return waitingVisits.stream()
+                .min(Comparator.comparing(Visit::getArrivalTime))
+                .orElse(null);
     }
+}
 
--- src/main/java/com/youcode/wrm/strategy/Impl/PriorityStrategy.java
+++ src/main/java/com/youcode/wrm/strategy/Impl/PriorityStrategy.java
@@ -1,4 +1,18 @@
 package com.youcode.wrm.strategy.Impl;
 
-public class PriorityStrategy {
+import com.youcode.wrm.entity.Visit;
+import com.youcode.wrm.strategy.QueueStrategy;
+
+import java.util.Comparator;
+import java.util.List;
+
+public class PriorityStrategy implements QueueStrategy {
+    @Override
+    public Visit getNextVisit(List<Visit> waitingVisits) {
+        return waitingVisits.stream()
+                .max(Comparator
+                        .comparing(Visit::getPriority)
+                        .thenComparing(Visit::getArrivalTime))
+                .orElse(null);
+    }
 }
